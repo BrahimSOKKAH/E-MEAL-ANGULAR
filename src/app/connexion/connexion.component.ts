0df3eb1f0364e45b38b620e640ec35033e03a7e0
Brahim_SOKKAH <brahim.sokkah@gmail.com>
2023-05-23 11:28:35 +0200

    creation connexion

---
+++ src/app/connexion/connexion.component.ts
@@ -0,0 +1,32 @@
+import { Component, EventEmitter, Output } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-connexion',
+  templateUrl: './connexion.component.html',
+  styleUrls: ['./connexion.component.css']
+})
+export class ConnexionComponent {
+  submitted : boolean = false
+  connectForm = this.fb.group(
+    {
+    email: ['', [Validators.required, Validators.email]],
+    password: ['', [Validators.required, Validators.minLength(5)]]
+  }
+  )
+
+  constructor(private fb: FormBuilder) {}
+
+  onSubmit = () => {
+    this.submitted = true
+    if(this.connectForm.valid) console.log(this.connectForm);
+  }
+
+  @Output()
+  click: EventEmitter<any> = new EventEmitter()
+
+
+  onClick = () => {
+    this.click.emit()
+  }
+}
