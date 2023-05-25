2f7fd7c00a38e6a61ce74370fbeb6bcdf907d572
Brahim_SOKKAH <brahim.sokkah@gmail.com>
2023-05-25 11:34:17 +0200

    ajoute partenaire

--- src/app/partenaires/models/PartenairesModel.ts
+++ src/app/partenaires/models/PartenairesModel.ts
@@ -1,15 +1,38 @@
-// partenaire.model.ts
-export class Partenaire {
+interface PartenaireInterface {
+    id: number;
+    nom: string;
+    role: string;
+    email: string;
+    password: string;
+    siteWeb: string;
+    logo: string;
+    description: string;
+  }
+  
+  class PartenaireModel implements PartenaireInterface {
     id!: number;
     nom!: string;
-    role: string;
+    role!: string;
     email!: string;
     password!: string;
     siteWeb!: string;
     logo!: string;
     description!: string;
-
-    constructor() {
-        this.role = "ROLE_PARTNER";
+  
+    constructor(nom?: string, role?: string, email?: string, password?: string, siteWeb?: string, logo?: string, description?: string) {
+      if (nom) this.nom = nom;
+      if (role) this.role = role;
+      if (email) this.email = email;
+      if (password) this.password = password;
+      if (siteWeb) this.siteWeb = siteWeb;
+      if (logo) this.logo = logo;
+      if (description) this.description = description;
     }
-}
+  
+    toString = () => {
+      return `Partenaire {id : ${this.id}, nom: ${this.nom}, role: ${this.role}, email: ${this.email}, password: ${this.password}, siteWeb: ${this.siteWeb}, logo: ${this.logo}, description: ${this.description}}`;
+    }
+  }
+  
+  export {PartenaireInterface, PartenaireModel};
+  
